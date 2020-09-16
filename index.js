0ce358c13d11cc926f98738869f2c3874d2a8b45
Kareem <Kareem@hismachine>
2020-09-16 10:46:09 +0300

    General Refactoring & fixing errors.
     - status !== 200 && 201
       JS has will evaluate 201 as true so this statement actually gets translated to
       if (status !== 200 && true). This means that if status =201, you will endup failing the plugin
    
    - Refactor fetch logic. The general consensus is that if we are using async await then we don't
      to use promise chaining.
    
    - use switch instead of multiple if/else to check for status code

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const fetch = require('node-fetch');
 const env = require('./env');
 
 // Configure the required API vars
-let authToken = env.BUILD_SITREP_TOKEN;
-let authString = 'Bearer ' + authToken
-let baseURL = `https://api.netlify.com/api/v1/sites/${env.SITE_ID}/snippets`
+const NETLIFY_APU_VERSION = 'v1';
+const authToken = env.BUILD_SITREP_TOKEN;
+const authString = 'Bearer ' + authToken
+const baseURL = `https://api.netlify.com/api/${NETLIFY_APU_VERSION}/sites/${env.SITE_ID}/snippets`
 
 const isValidString = (value)=> !!value&&value!=='';
 const Logger = {
@@ -21,21 +22,27 @@ const PRODUCTION = 'production';
 
 module.exports = {
 
-    // Note: we could do all of this in a single event handler (e.g. onSuccess) but
-    // we split it up to save as much execution time as possible in the event of
-    // an error or misconfiguration.
-
+    /**
+     * NOTE:
+     * we could do all of this in a single event handler (e.g. onSuccess) but
+     * we split it up to save as much execution time as possible in the event of
+     * an error or misconfiguration.
+     */
     onPreBuild: ({ inputs, utils: { build: { failPlugin } } }) => {
         // Check inputs
         global.verbose = inputs.verbose;
-        let allowProd = inputs.allow_prod;
+        const allowProd = inputs.allow_prod;
         Logger.debug('Verbose mode enabled');
-        // Make sure the token has been set
-        // Hopefully we get access to this via build in the future
+        /**
+         * Make sure the token has been set
+         * Hopefully we get access to this via build in the future
+         */
         if (isValidString(authToken)) {
           return failPlugin('The BUILD_SITREP_TOKEN environment var has not been set.');
         }
-        // Make sure we don't run in disallowed contexts
+        /**
+         * Make sure we don't run in disallowed contexts
+         */
         Logger.debug('Verifying allowable contexts...');
         if (!allowProd && env.CONTEXT === PRODUCTION) {
             return failPlugin('Production debug is disabled. This can be changed in netlify.toml');
@@ -44,70 +51,56 @@ module.exports = {
     },
     onBuild({ utils: { build: { failPlugin } } }) {
         Logger.debug('Building the tag UI...');
-
-        let data = {build_id: env.BUILD_ID, context: env.CONTEXT, commit_ref: env.COMMIT_REF, deploy_id: env.DEPLOY_ID};
+        const data = {build_id: env.BUILD_ID, context: env.CONTEXT, commit_ref: env.COMMIT_REF, deploy_id: env.DEPLOY_ID};
         ejs.renderFile(__dirname + '/templates/template.ejs', data, function(err, data) {
             if (err) {
                 return failPlugin ('Something went wrong when processing the display template: ' + err);
             }
-
-            let renderToBase64 = new Buffer.from(data);
-            let encodedRender = renderToBase64.toString('base64');
-
-            // This looks insane, but since snippet injection isn't exposed
-            // to build plugins, we have to use the API and pass data that
-            // can run without further processing. The only alternative is
-            // to modify the filesystem.
-            let tagOpen =   '<script>' +
+            const renderToBase64 = new Buffer.from(data);
+            const encodedRender = renderToBase64.toString('base64');
+            /**
+             * This looks insane, but since snippet injection isn't exposed
+             * to build plugins, we have to use the API and pass data that
+             * can run without further processing. The only alternative is
+             * to modify the filesystem.
+             */
+            const tagOpen =   '<script>' +
                             'window.onload = function() {' +
                             'var ifrm = document.createElement(\'iframe\');' +
                             'ifrm.setAttribute(\'id\', \'ifrm\');' +
                             'ifrm.setAttribute(\'style\', \'position: fixed; bottom: 0; right: 0; border: 0;\');' +
                             'document.body.appendChild(ifrm);'
-
-            let tagData =   'ifrm.setAttribute(\'src\', \'data:text/html;base64,' + encodedRender + '\');'
-            let tagClose =    '}' +
+            const tagData =   'ifrm.setAttribute(\'src\', \'data:text/html;base64,' + encodedRender + '\');'
+            const tagClose =    '}' +
                             '</script>'
-
-        global.tagComplete = tagOpen + tagData + tagClose
-        if (verbose) console.log('Successfully built the tag UI.')
-
+            global.tagComplete = tagOpen + tagData + tagClose
+            Logger.debug('Successfully built the tag UI.')
         });
         },
 
     async onSuccess({ inputs, utils: { build: { failPlugin, failBuild }, status: { show } } }) {
-
-        if (verbose) console.log('Preparing snippet injection...');
-
         try {
+            Logger.debug('Preparing snippet injection...');
+            Logger.debug('Checking to see if a snippet already exists...');
+            const opts = {
+                extendedURL:undefined,
+                fetchMethod:undefined
+            };
+            const response = await fetch(baseURL, { method: 'GET', headers: {'Authorization': authString}});
+            const snippets = await response.json().filter((item)=>(item.title === "netlify-build-sitrep"));
+            if(!!snippets){
+                Logger.debug('Couldn\'t find an existing snippet.');
+                opts.extendedURL=baseURL;
+                opts.fetchMethod='POST';
+            }else{
+                const snippetID = snippets[0].id;
+                opts.extendedURL = baseURL + '/' + snippetID;
+                opts.fetchMethod='PUT';
+                Logger.debug('Found snippet with ID: '+snippetID);
+            };
 
-            if (verbose) console.log('Checking to see if a snippet already exists...');
-
-            await fetch(baseURL, { method: 'GET', headers: {'Authorization': authString}})
-              .then((res) => {
-                 return res.json()
-            })
-
-            .then((json) => {
-                let findSnippet = json.filter(function(item) {
-                    return item.title == "netlify-build-sitrep";
-                });
-
-                // TODO: this is super sketchy, needs to be addressed
-                if (findSnippet === undefined || findSnippet.length == 0) {
-                    if (verbose) console.log('Couldn\'t find an existing snippet.');
-                    extendedURL = baseURL;
-                    fetchMethod = 'POST'
-                } else {
-                    let snippetID = findSnippet[0]['id'];
-                    extendedURL = baseURL + '/' + snippetID;
-                    if (verbose) console.log('Found snippet with id: ' + snippetID);
-                    fetchMethod = 'PUT'
-                }
-            })
-
-            const { status, statusText } = await fetch(extendedURL, {
-                method: fetchMethod,
+            const { status, statusText } = await fetch(opts.extendedURL, {
+                method: opts.fetchMethod,
                 headers: {
                     'Authorization': authString,
                     "Content-type": "application/json",
@@ -120,20 +113,25 @@ module.exports = {
                     general_position: "body",
                 })
             })
-
-            if (verbose && (status == 201)) {
-                console.log('Snippet was added.');
-            } else if (verbose && (status == 200)) {
-                console.log('Snippet was updated.');
-            } else if (status !== 200 && 201) {
-                return failPlugin('Snippet injection failed on an API error. Netlify said: ' + status + statusText);
+            switch (status) {
+                case 201:
+                    Logger.debug('Snippet was added.');
+                    break;
+                case 200:
+                    Logger.debug('Snippet was updated.');
+                    break;
+                default:
+                    return failPlugin('Snippet injection failed on an API error. Netlify said: ' + status + statusText);
             }
 
             return show ({
               title: 'Sitrep injected successfully',
               summary: 'You should be able to see it on ' + env.DEPLOY_URL,
-              text: 'Build ID: ' + env.BUILD_ID + ' | Site ID: ' + env.SITE_ID + ' | Commit: ' + env.COMMIT_REF + ' | Deploy ID: ' + env.DEPLOY_ID + '',
-            })
+              text: 'Build ID: ' + env.BUILD_ID +
+                  ' | Site ID: ' + env.SITE_ID +
+                  ' | Commit: ' + env.COMMIT_REF +
+                  ' | Deploy ID: ' + env.DEPLOY_ID,
+            });
         }
           catch (error) {
             return failBuild('Something catastrophic happened.', { error })
